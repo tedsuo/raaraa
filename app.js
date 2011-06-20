9409ba59a738f5350fd7eb7893d2a8dba866bd16
William Budington <bill.budington@gmail.com>
2011-06-20 11:16:36 -0700

    Starting app with express engine.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-c = require('connect');
+var express = require('express');
 
-c(
-  c.static(__dirname+'/client')
-).listen(9002);
+var app = express.createServer(express.logger(), express.bodyParser());
 
-console.log('RaaRaa litening on port 9002');
+app.get('/', function(req, res){
+	res.end();
+	//res.render('index.jade', { title: 'My Site' });
+});
+
+app.configure(function(
+	app.use(express.static(__dirname + '/public/old'));
+	app.use(express.cookieParser());
+	app.use(express.session());
+));
+
+app.listen(9002);
+
+console.log('RaaRaa listening on port 9002');

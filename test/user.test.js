00ccaf2561553975bcb9a8ffaed26b3b53828e90
Byron Young <titus.byronicus@gmail.com>
2011-12-11 20:25:53 -0800

    Fixed user.test.js failing to quit (forgot to close the database connection)

--- test/user.test.js
+++ test/user.test.js
@@ -1,36 +1,47 @@
 process.env.NODE_ENV = 'test';
 
 var rr = require("../raaraa"),
-    db = require("../raaraa/db");
-
-db.collection("users").remove();
+db = require("../raaraa/db");
 
 module.exports = {
-  
-  "create account with username/password": function(test) {
-  
-    test.expect(5);
-    
-    // key for creating and retrieving the account
-    var USER_ID = {
-      username: 'test_'+Date.now(),
-      password: 'password'
-    };
-
-    // create a new user with username and password combo
-    rr.createUser( USER_ID, function(err,created_user){
-        test.ok(err === null, err);
-        test.ok(created_user, "no user created");
-
-        // try to find the user with the same combo
-        rr.findUser( USER_ID, function(err,found_user) {
-            test.ok(err === null, err);
-            test.ok(found_user, "no user found");
-
-            // both variables must reference the exact same object
-            test.deepEqual(created_user, found_user);
-            test.done();  
+    setUp: function(next) {
+        db.collection("users").remove().done(function(){
+            next();
         });
-    });
-  },
+    },
+    
+    "create account with username/password": function(test) {
+        
+        test.expect(5);
+        
+        // key for creating and retrieving the account
+        var USER_ID = {
+            username: 'test_'+Date.now(),
+            password: 'password'
+        };
+
+        // create a new user with username and password combo
+        rr.createUser( USER_ID, function(err,created_user){
+            test.ok(err === null, err);
+            test.ok(created_user, "no user created");
+
+            // try to find the user with the same combo
+            rr.findUser( USER_ID, function(err,found_user) {
+                test.ok(err === null, err);
+                test.ok(found_user, "no user found");
+
+                // both variables must reference the exact same object
+                test.deepEqual(created_user, found_user);
+                console.log("done with DB stuff");
+                process.nextTick(function() {
+                    console.log("ending test");
+                    test.done();
+                });
+            });
+        });
+    },
+
+    tearDown: function(next) {
+        db.close(next);
+    }
 };

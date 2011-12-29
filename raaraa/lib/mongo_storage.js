e10a531c24b685111e053d49b6355af24720fe26
Byron Young <titus.byronicus@gmail.com>
2011-12-28 19:41:07 -0800

    We can't just use ID strings directly, we have to convert them to BSON.ObjectID objects before using them in queries.

--- raaraa/lib/mongo_storage.js
+++ raaraa/lib/mongo_storage.js
@@ -1,78 +1,89 @@
 var DataStorage = require('./storage'),
-    _ = require("underscore"),
-    db = require("../db");
-    
-    console.log(DataStorage);
-    
+_ = require("underscore"),
+db = require("../db"),
+BSON = require("mongoq").BSON;
+
+console.log(DataStorage);
+
 var MongoStorage = module.exports = function MongoStorage(options) {
-    DataStorage.apply(this, arguments);
+  DataStorage.apply(this, arguments);
 };
 
+var fixupID = function(model) {
+  var id = model.id;
+  if (!(id instanceof BSON.ObjectID)) {
+    id = BSON.ObjectID(id);
+  }
+  return id;
+}
+
 _.extend(MongoStorage.prototype, DataStorage.prototype, {
-    init: function(options) {
-        DataStorage.prototype.init.call(this, options);
-        this.collectionName = options.collectionName;
-        this.collection = db.collection(this.collectionName);
-    },
+  init: function(options) {
+    DataStorage.prototype.init.call(this, options);
+    this.collectionName = options.collectionName;
+    this.collection = db.collection(this.collectionName);
+  },
 
-    query: function(queryParams, cb) {
-        this.collection.find(queryParams).toArray()
-            .done(function(items) {
-              debugger;
-                cb(null, items);
-            })
-            .fail(function(err) {
-                cb(err, null);
-            });
-    },
+  query: function(queryParams, cb) {
+    if (queryParams._id) {
+      queryParams._id = fixupID({ id: queryParams._id });
+    }
+    this.collection.find(queryParams).toArray()
+      .done(function(items) {
+        cb(null, items);
+      })
+      .fail(function(err) {
+        cb(err, null);
+      });
+  },
 
-    insert: function(model, cb) {
-        this.collection.insert(model.toJSON(), { safe: true })
-            .done(function(item) {
-                cb(null, item);
-            })
-            .fail(function(err) {
-                cb(err, null);
-            });
-    },
+  insert: function(model, cb) {
+    this.collection.insert(model.toJSON(), { safe: true })
+      .done(function(item) {
+        cb(null, item);
+      })
+      .fail(function(err) {
+        cb(err, null);
+      });
+  },
 
-    update: function(model, cb) {
-        this.collection.update({ _id: model.id },
-                               model.toJSON(),
-                               { safe: true })
-            .done(function() {
-                cb(null, model.toJSON());
-            })
-            .fail(function(err) {
-                cb(err, null);
-            });
-    },
+  update: function(model, cb) {
+    this.collection.update({ _id: fixupID(model) },
+                           model.toJSON(),
+                           { safe: true })
+      .done(function() {
+        cb(null, model.toJSON());
+      })
+      .fail(function(err) {
+        cb(err, null);
+      });
+  },
 
-    delete: function(model, cb) {
-        this.collection.remove({ _id: model.id })
-            .done(function() {
-                cb(null, null);
-            })
-            .fail(function(err) {
-                cb(err, null);
-            });
-    },
+  delete: function(model, cb) {
+    this.collection.remove({ _id: fixupID(model) })
+      .done(function() {
+        cb(null, null);
+      })
+      .fail(function(err) {
+        cb(err, null);
+      });
+  },
 
-    parseModel: function(response) {
-        if (response instanceof Array) {
-            return response[0];
-        } else {
-            return response;
-        }
-    },
+  parseModel: function(response) {
+    if (response instanceof Array) {
+      return response[0];
+    } else {
+      return response;
+    }
+  },
 
-    parseDataView: function(response) {
-        if (response instanceof Array) {
-            return response;
-        } else {
-            return [response];
-        }
-    },
+  parseDataView: function(response) {
+    if (response instanceof Array) {
+      return response;
+    } else {
+      return [response];
+    }
+  },
 });
 
 MongoStorage.prototype.__proto__ = DataStorage.prototype;

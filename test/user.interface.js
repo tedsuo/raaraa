4d346a17561aaa5616249221ca70105f93070e22
Byron Young <titus.byronicus@gmail.com>
2011-12-28 21:02:33 -0800

    More user browser tests * test log out and log in * test passwords don't match in sign up form

--- test/user.interface.js
+++ test/user.interface.js
@@ -1,18 +1,47 @@
 process.env.NODE_ENV = 'test';
 
 var db = require("../raaraa").db,
-  server = require("../server"),
-  zombie = require('zombie'),
-  userFixtures = require("./fixtures/users"),
-  testMaker = require("./lib/testmaker");
+server = require("../server"),
+zombie = require('zombie'),
+user_fixtures = require("./fixtures/users"),
+testMaker = require("./lib/testmaker");
 
+var sign_up_user = user_fixtures.randomUser();
+
+var _last_browser;
 function createBrowser() {
-  return new zombie.Browser({
+  _last_browser = new zombie.Browser({
     site: 'http://'+server.host+':'+server.port,
-    debug: true
+    debug: false
+  });
+  return _last_browser;
+}
+
+function lastBrowser() {
+  return _last_browser;
+}
+
+function signUp(user, browser, cb) {
+  browser.visit('/', function(e, browser) {
+    browser
+      .fill('#signup-user',user.username)
+      .fill('#signup-pass',user.password)
+      .fill('#signup-verify',user.password_verify || user.password)
+      .pressButton('#signup-submit', function(e, browser) {
+        _last_browser = browser;
+        cb(e, browser);
+      });
   });
 }
 
+function logIn(user, browser, cb) {
+  browser.visit('/', function(e, browser) {
+    browser
+      .fill('#signin-user',user.username)
+      .fill('#signin-pass',user.password)
+      .pressButton('#signin-submit', cb);
+  });
+}
 
 module.exports = testMaker({
   beforeAll: function(next) {
@@ -46,34 +75,101 @@ module.exports = testMaker({
       });
     },
     
-    "sign up from homepage": function(test) {
-      test.expect(4);
-      
-      var user = userFixtures.randomUser();
-      var browser = createBrowser();
-      browser.visit('/', function(e, browser) {
-        test.ok(browser.success, "Failed to load homepage");
-        browser
-          .fill('#signup-user',user.username)
-          .fill('#signup-pass',user.password)
-          .fill('#signup-verify',user.password)
-          .pressButton('#signup-submit', function(e, browser) {
+    "sign up duplicate user": function(test) {
+      test.expect(1);
 
-            // Form submitted, new page loaded.
-            test.ok(browser.success,
-                      browser.statusCode+" Error: "+browser.html());
-            if (browser.error) {
-              test.ifError(browser.error);
-            } else {
-              test.ok(!browser.html("#signup"),
-                        "We should not be seeing the login page");
-              test.ok(browser.html("#logout-btn"),
-                        "We should be seeing a logout button");
-            }
-            test.done();
-          });
+      var user = user_fixtures.randomUser();
+      db.collection("users").insert(user).done(function(user) {
+        var browser = createBrowser();
+        signUp(user, browser, function(e, browser) {
+          test.equal(browser.text("#errors"),
+                     "Error: User already exists");
+          test.done();
+        });
       });
-    }
+    },
+
+    "sign up form not valid": function(test) {
+      test.expect(1);
+
+      var user = user_fixtures.randomUser();
+      user.password_verify = 'asdf';
+      var browser = createBrowser();
+
+      signUp(user, browser, function(e, browser) {
+          test.equal(browser.text("#errors"),
+                     "Error: Passwords do not match");
+          test.done();
+      });
+    },
+
+    "sign up from homepage": function(test) {
+      test.expect(3);
+      
+      var browser = createBrowser();
+
+      signUp(sign_up_user, browser, function(e, browser) {
+        // Form submitted, new page loaded.
+         test.ok(browser.success,
+                browser.statusCode+" Error: "+browser.html());
+
+        if (browser.error) {
+          test.ifError(browser.error);
+          return test.done();
+        }
+
+        test.ok(!browser.html("#signup"),
+                "We should not be seeing the login page");
+        test.ok(browser.html("#logout-btn"),
+                "We should be seeing a logout button");
+
+        test.done();
+      });
+    },
+
+    "log out": function(test) {
+      test.expect(3);
+
+      lastBrowser().clickLink("#logout-btn", function(e, browser) {
+        // #TODO: WTF!? why is browser location now 'http://localhost:9002/signup'?
+        console.log(browser.location.toString());
+
+        test.ok(browser.success,
+                browser.statusCode+" Error: "+browser.html());
+
+        if(browser.error) {
+          test.ifError(browser.error);
+          return test.done();
+        }
+
+        test.ok(browser.html("#signup"),
+                "We should be seeing the login page");
+        test.ok(!browser.html("#logout-btn"),
+                "We should not be seeing a logout button");
+        test.done();
+      });
+    },
+
+    "log in": function(test) {
+      test.expect(3);
+
+      logIn(sign_up_user, createBrowser(), function(e, browser) {
+        test.ok(browser.success,
+                browser.statusCode+" Error: "+browser.html());
+
+        if(browser.error) {
+          test.ifError(browser.error);
+          return test.done();
+        }
+
+        test.ok(!browser.html("#signup"),
+                "We should not be seeing the login page");
+        test.ok(browser.html("#logout-btn"),
+                "We should be seeing a logout button");
+        test.done();
+      });
+    },
+
   }
 });
 
